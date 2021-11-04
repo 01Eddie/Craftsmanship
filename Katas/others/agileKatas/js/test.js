a2f0948e41d9385993c487c61044b15e4c5b314d
01Eddie <jesus.11.97@hotmail.com>
2021-11-04 14:02:32 -0500

    [ADD] test.js

--- Katas/others/agileKatas/js/test.js
+++ Katas/others/agileKatas/js/test.js
@@ -1,8 +1,17 @@
-let {BingoCards} = require('./bingo')
+let { BingoCards } = require('./bingo')
 let assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+    it(`BingoCards {
+      checked: [],
+      columns: [
+        [ 1, 2, 3, 4, 5 ],
+        [ 11, 22, 33, 44, 55 ],
+        [ 21, 12, 23, 34, 45 ],
+        [ 9, 8, 7, 6, 1 ],
+        [ 1, 2, 32, 45, 51 ]
+      ]
+    }`, function() {
       column = [
         [ 1, 2, 3, 4, 5 ],
         [ 11, 22, 33, 44, 55 ],
