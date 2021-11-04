4b98902680e0a3a0810fd622ad48a647b413dc23
01Eddie <jesus.11.97@hotmail.com>
2021-11-04 15:40:15 -0500

    [ADD] file test

--- Katas/others/agileKatas/js/bingo.js
+++ Katas/others/agileKatas/js/bingo.js
@@ -114,11 +114,4 @@ function generate_random_number(lower_bound, upper_bound){
     return Math.random() * (upper_bound - lower_bound) + lower_bound;
 }
 
-
-const columns = [["B",1, 2, 3, 4, 5],
-["I",11, 22, 33, 44, 55],
-["N",21, 12, 23, 34, 45],
-["G",9, 8, 7, 6, 1],
-["O",1, 2, 32, 45, 51]]
-console.log(new BingoCards(columns));
 module.exports = { BingoCards };
